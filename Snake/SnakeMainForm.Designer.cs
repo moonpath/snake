2a2b830ee618b696ee95f979711c34a6129a95a6
hongyuzhang <moonpath@foxmail.com>
2018-12-06 00:28:46 +0800

    initial commit

---
+++ Snake/SnakeMainForm.Designer.cs
@@ -0,0 +1,249 @@
+﻿namespace Snake
+{
+    partial class SnakeMainForm
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(SnakeMainForm));
+            this.run = new System.Windows.Forms.Timer(this.components);
+            this.MainPanel = new System.Windows.Forms.Panel();
+            this.GameMenu = new System.Windows.Forms.MenuStrip();
+            this.GameToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.StartToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.EndToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.HelpToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.IntroductionToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.AboutToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.SubPanel = new System.Windows.Forms.Panel();
+            this.StartButton = new System.Windows.Forms.Button();
+            this.LevelBox = new System.Windows.Forms.TextBox();
+            this.CurrentScoreBox = new System.Windows.Forms.TextBox();
+            this.HighestScoreBox = new System.Windows.Forms.TextBox();
+            this.LevelLabel = new System.Windows.Forms.Label();
+            this.CurrentScoreLabel = new System.Windows.Forms.Label();
+            this.HighestScoreLabel = new System.Windows.Forms.Label();
+            this.GameMenu.SuspendLayout();
+            this.SubPanel.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // run
+            // 
+            this.run.Interval = 200;
+            this.run.Tick += new System.EventHandler(this.run_Tick);
+            // 
+            // MainPanel
+            // 
+            this.MainPanel.BackColor = System.Drawing.Color.Gainsboro;
+            this.MainPanel.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.MainPanel.Location = new System.Drawing.Point(3, 28);
+            this.MainPanel.Name = "MainPanel";
+            this.MainPanel.Size = new System.Drawing.Size(503, 503);
+            this.MainPanel.TabIndex = 0;
+            // 
+            // GameMenu
+            // 
+            this.GameMenu.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.GameToolStripMenuItem,
+            this.HelpToolStripMenuItem});
+            this.GameMenu.Location = new System.Drawing.Point(0, 0);
+            this.GameMenu.Name = "GameMenu";
+            this.GameMenu.Size = new System.Drawing.Size(641, 25);
+            this.GameMenu.TabIndex = 1;
+            this.GameMenu.Text = "menuStrip1";
+            // 
+            // GameToolStripMenuItem
+            // 
+            this.GameToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.StartToolStripMenuItem,
+            this.EndToolStripMenuItem});
+            this.GameToolStripMenuItem.Name = "GameToolStripMenuItem";
+            this.GameToolStripMenuItem.Size = new System.Drawing.Size(44, 21);
+            this.GameToolStripMenuItem.Text = "游戏";
+            // 
+            // StartToolStripMenuItem
+            // 
+            this.StartToolStripMenuItem.Name = "StartToolStripMenuItem";
+            this.StartToolStripMenuItem.Size = new System.Drawing.Size(100, 22);
+            this.StartToolStripMenuItem.Text = "开始";
+            this.StartToolStripMenuItem.Click += new System.EventHandler(this.StartToolStripMenuItem_Click);
+            // 
+            // EndToolStripMenuItem
+            // 
+            this.EndToolStripMenuItem.Name = "EndToolStripMenuItem";
+            this.EndToolStripMenuItem.Size = new System.Drawing.Size(100, 22);
+            this.EndToolStripMenuItem.Text = "退出";
+            this.EndToolStripMenuItem.Click += new System.EventHandler(this.EndToolStripMenuItem_Click);
+            // 
+            // HelpToolStripMenuItem
+            // 
+            this.HelpToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.IntroductionToolStripMenuItem,
+            this.AboutToolStripMenuItem});
+            this.HelpToolStripMenuItem.Name = "HelpToolStripMenuItem";
+            this.HelpToolStripMenuItem.Size = new System.Drawing.Size(44, 21);
+            this.HelpToolStripMenuItem.Text = "帮助";
+            // 
+            // IntroductionToolStripMenuItem
+            // 
+            this.IntroductionToolStripMenuItem.Name = "IntroductionToolStripMenuItem";
+            this.IntroductionToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+            this.IntroductionToolStripMenuItem.Text = "说明";
+            this.IntroductionToolStripMenuItem.Click += new System.EventHandler(this.IntroductionToolStripMenuItem_Click);
+            // 
+            // AboutToolStripMenuItem
+            // 
+            this.AboutToolStripMenuItem.Name = "AboutToolStripMenuItem";
+            this.AboutToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+            this.AboutToolStripMenuItem.Text = "关于";
+            this.AboutToolStripMenuItem.Click += new System.EventHandler(this.AboutToolStripMenuItem_Click);
+            // 
+            // SubPanel
+            // 
+            this.SubPanel.BackColor = System.Drawing.Color.Gainsboro;
+            this.SubPanel.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.SubPanel.Controls.Add(this.StartButton);
+            this.SubPanel.Controls.Add(this.LevelBox);
+            this.SubPanel.Controls.Add(this.CurrentScoreBox);
+            this.SubPanel.Controls.Add(this.HighestScoreBox);
+            this.SubPanel.Controls.Add(this.LevelLabel);
+            this.SubPanel.Controls.Add(this.CurrentScoreLabel);
+            this.SubPanel.Controls.Add(this.HighestScoreLabel);
+            this.SubPanel.Location = new System.Drawing.Point(509, 28);
+            this.SubPanel.Name = "SubPanel";
+            this.SubPanel.Size = new System.Drawing.Size(129, 503);
+            this.SubPanel.TabIndex = 2;
+            // 
+            // StartButton
+            // 
+            this.StartButton.Location = new System.Drawing.Point(27, 424);
+            this.StartButton.Name = "StartButton";
+            this.StartButton.Size = new System.Drawing.Size(75, 23);
+            this.StartButton.TabIndex = 0;
+            this.StartButton.Text = "开始";
+            this.StartButton.UseVisualStyleBackColor = true;
+            this.StartButton.Click += new System.EventHandler(this.StartButton_Click);
+            // 
+            // LevelBox
+            // 
+            this.LevelBox.Location = new System.Drawing.Point(73, 259);
+            this.LevelBox.Name = "LevelBox";
+            this.LevelBox.ReadOnly = true;
+            this.LevelBox.Size = new System.Drawing.Size(29, 21);
+            this.LevelBox.TabIndex = 5;
+            // 
+            // CurrentScoreBox
+            // 
+            this.CurrentScoreBox.Location = new System.Drawing.Point(73, 171);
+            this.CurrentScoreBox.Name = "CurrentScoreBox";
+            this.CurrentScoreBox.ReadOnly = true;
+            this.CurrentScoreBox.Size = new System.Drawing.Size(29, 21);
+            this.CurrentScoreBox.TabIndex = 4;
+            // 
+            // HighestScoreBox
+            // 
+            this.HighestScoreBox.Location = new System.Drawing.Point(73, 80);
+            this.HighestScoreBox.Name = "HighestScoreBox";
+            this.HighestScoreBox.ReadOnly = true;
+            this.HighestScoreBox.Size = new System.Drawing.Size(29, 21);
+            this.HighestScoreBox.TabIndex = 3;
+            // 
+            // LevelLabel
+            // 
+            this.LevelLabel.AutoSize = true;
+            this.LevelLabel.Location = new System.Drawing.Point(17, 262);
+            this.LevelLabel.Name = "LevelLabel";
+            this.LevelLabel.Size = new System.Drawing.Size(65, 12);
+            this.LevelLabel.TabIndex = 2;
+            this.LevelLabel.Text = "当前等级：";
+            // 
+            // CurrentScoreLabel
+            // 
+            this.CurrentScoreLabel.AutoSize = true;
+            this.CurrentScoreLabel.Location = new System.Drawing.Point(17, 174);
+            this.CurrentScoreLabel.Name = "CurrentScoreLabel";
+            this.CurrentScoreLabel.Size = new System.Drawing.Size(65, 12);
+            this.CurrentScoreLabel.TabIndex = 1;
+            this.CurrentScoreLabel.Text = "当前分数：";
+            // 
+            // HighestScoreLabel
+            // 
+            this.HighestScoreLabel.AutoSize = true;
+            this.HighestScoreLabel.Location = new System.Drawing.Point(17, 83);
+            this.HighestScoreLabel.Name = "HighestScoreLabel";
+            this.HighestScoreLabel.Size = new System.Drawing.Size(65, 12);
+            this.HighestScoreLabel.TabIndex = 0;
+            this.HighestScoreLabel.Text = "最高分数：";
+            // 
+            // SnakeMainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.WhiteSmoke;
+            this.ClientSize = new System.Drawing.Size(641, 534);
+            this.Controls.Add(this.SubPanel);
+            this.Controls.Add(this.MainPanel);
+            this.Controls.Add(this.GameMenu);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MainMenuStrip = this.GameMenu;
+            this.MaximizeBox = false;
+            this.Name = "SnakeMainForm";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "贪吃蛇";
+            this.Load += new System.EventHandler(this.SnakeMainForm_Load);
+            this.GameMenu.ResumeLayout(false);
+            this.GameMenu.PerformLayout();
+            this.SubPanel.ResumeLayout(false);
+            this.SubPanel.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Timer run;
+        private System.Windows.Forms.Panel MainPanel;
+        private System.Windows.Forms.MenuStrip GameMenu;
+        private System.Windows.Forms.ToolStripMenuItem GameToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem StartToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem HelpToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem IntroductionToolStripMenuItem;
+        private System.Windows.Forms.Panel SubPanel;
+        private System.Windows.Forms.Button StartButton;
+        private System.Windows.Forms.TextBox LevelBox;
+        private System.Windows.Forms.TextBox CurrentScoreBox;
+        private System.Windows.Forms.TextBox HighestScoreBox;
+        private System.Windows.Forms.Label LevelLabel;
+        private System.Windows.Forms.Label CurrentScoreLabel;
+        private System.Windows.Forms.Label HighestScoreLabel;
+        private System.Windows.Forms.ToolStripMenuItem EndToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem AboutToolStripMenuItem;
+    }
+}
+
---
+++ Snake/SnakeMainForm.cs
@@ -0,0 +1,327 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Collections;
+using System.IO;
+using System.Media;
+
+namespace Snake
+{
+    public partial class SnakeMainForm : Form
+    {
+        const int MAPSIZE = 25;
+        const int SHOWSIZE = 20;
+        const int OFFSET = 0;
+        const int INITIALLENGTH=2;
+        Color SNAKECOLOR = Color.Blue;
+        Color BACKGROUNDCOLOR = Color.Gainsboro;
+        SoundPlayer bgm = new SoundPlayer(Snake.Properties.Resources.BGM);
+        Random foodIndex = new Random();
+        Queue body = new Queue();
+        Queue direction = new Queue();
+        int currentDirection = 0;
+        int nextDirection = -1;
+        int food;
+        int head = MAPSIZE / 2 * 100 + MAPSIZE / 2;
+        int suspandTimes = 2;
+
+        PictureBox[,] map = new PictureBox[MAPSIZE, MAPSIZE];
+        public SnakeMainForm()
+        {
+            InitializeComponent();
+        }
+
+        private void SnakeMainForm_Load(object sender, EventArgs e)
+        {
+            System.IO.Directory.SetCurrentDirectory(System.AppDomain.CurrentDomain.SetupInformation.ApplicationBase);
+            for (int i = 0; i < MAPSIZE; i++)
+                for (int j = 0; j < MAPSIZE; j++)
+                {
+                    map[i, j] = new PictureBox();
+                    map[i, j].SizeMode = PictureBoxSizeMode.StretchImage;
+                    //map[i, j].BorderStyle = BorderStyle.Fixed3D;
+                    map[i, j].BackColor = BACKGROUNDCOLOR;
+                    map[i, j].Top = i * SHOWSIZE + OFFSET;
+                    map[i, j].Left = j * SHOWSIZE + OFFSET;
+                    map[i, j].Width = SHOWSIZE;
+                    map[i, j].Height = SHOWSIZE;
+                    MainPanel.Controls.Add(map[i, j]);
+                }
+            HighestScoreBox.Text = GetRecord().ToString();
+            CurrentScoreBox.Text = "0";
+            LevelBox.Text = "1";
+            body.Enqueue(head);
+            map[head/100, head%100].BackColor = SNAKECOLOR;
+            body.Enqueue(++head);
+            map[head / 100, head % 100].BackColor = SNAKECOLOR;
+            SetFood();
+        }
+        private int GetRecord()
+        {
+            int scoreRecord = 0;
+            try
+            {
+                StreamReader scoreRecordFile = new StreamReader(@"Record.rec");
+                scoreRecord = int.Parse(scoreRecordFile.ReadLine());
+                scoreRecordFile.Close();
+            }
+            catch
+            {
+            }
+            return scoreRecord;
+        }
+        private void SetRecord(int score)
+        {
+            try
+            {
+                StreamWriter scoreRecordFile = new StreamWriter(@"Record.rec");
+                scoreRecordFile.Write(score);
+                scoreRecordFile.Close();
+            }
+            catch
+            {
+            }
+        }
+
+        private void run_Tick(object sender, EventArgs e)
+        {
+            if(direction.Count!=0)
+                currentDirection = (int)direction.Dequeue();
+            int head_i = head / 100;
+            int head_j = head % 100;
+            if (currentDirection == 0)//right
+            {
+                ++head_j;
+            }
+            else if(currentDirection == 1)//down
+            {
+                ++head_i;
+            }
+            else if (currentDirection == 2)//left
+            {
+                --head_j;
+            }
+            else if (currentDirection == 3)//up
+            {
+                --head_i;
+            }
+            if (!Judge(head_i, head_j))
+            {
+                InitializeGame();
+                return;
+            }
+            map[head_i, head_j].BackColor = SNAKECOLOR;
+            head = head_i * 100 + head_j;
+            body.Enqueue(head);
+            if (head != food)
+            {
+                int tail = (int)body.Dequeue();
+                map[tail / 100, tail % 100].BackColor = BACKGROUNDCOLOR;
+            }
+            else if (!SetFood())
+            {
+                run.Enabled = false;
+                MessageBox.Show("恭喜，你已经达到最高分数！", "梦蛇", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                InitializeGame();
+                return;
+            }
+            else
+            {
+                CurrentScoreBox.Text = (body.Count - INITIALLENGTH).ToString();
+                if ((body.Count - INITIALLENGTH) % 8 == 0 && body.Count- INITIALLENGTH < 64)
+                {
+                    LevelBox.Text = (int.Parse(LevelBox.Text) + 1).ToString();
+                    run.Interval = run.Interval * 5 / 6;
+                }
+                else if (body.Count- INITIALLENGTH >= 64 && run.Interval != 200)
+                {
+                    LevelBox.Text = "MAX";
+                    run.Interval = 200;
+                }
+                System.Media.SystemSounds.Beep.Play();
+            }
+
+            //MessageBox.Show("OK");
+        }
+        private bool SetFood()
+        {
+            int[] foodInterval = new int[MAPSIZE * MAPSIZE];
+            int validLength = 0;
+            for (int i = 0; i < MAPSIZE; i++)
+                for (int j = 0; j < MAPSIZE; j++)
+                {
+                    int coordinate = i * 100 + j;
+                    if (!body.Contains(coordinate))
+                        foodInterval[validLength++] = coordinate;
+                }
+            if (validLength <= MAPSIZE * MAPSIZE / 2)
+                return false;
+            food = foodInterval[foodIndex.Next(0, validLength)];
+            map[food / 100, food % 100].BackColor = Color.Red;
+            return true;
+        }
+        private bool Judge(int coordinate_i, int coordinate_j)
+        {
+            if (coordinate_i < 0 || coordinate_i >= MAPSIZE || coordinate_j < 0 || coordinate_j >= MAPSIZE || body.Contains(coordinate_i * 100 + coordinate_j))
+            {
+                run.Enabled = false;
+                bgm.Stop();
+                int scoreRecord = GetRecord();
+                if(scoreRecord< body.Count- INITIALLENGTH)
+                {
+                    SetRecord(body.Count- INITIALLENGTH);
+                    MessageBox.Show("新纪录！您的得分为："+ (body.Count- INITIALLENGTH), "梦蛇", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                }
+                else
+                    MessageBox.Show(FailShow() + System.Environment.NewLine + "游戏结束！您的得分为：" + (body.Count- INITIALLENGTH), "梦蛇", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                return false;
+            }
+            return true;
+        }
+        protected override bool ProcessCmdKey(ref Message msg, Keys keyData)
+        {
+            switch (keyData)
+            {
+                case Keys.Right:
+                    if (currentDirection != 2 && direction.Count == 0)
+                    {
+                        direction.Enqueue(0);
+                        nextDirection = 0;
+                    }
+                    else if(nextDirection!= 2 && direction.Count == 1)
+                    {
+                        direction.Enqueue(0);
+                        nextDirection = 0;
+                    }
+                    break;
+                case Keys.Down:
+                    if (currentDirection != 3 && direction.Count == 0)
+                    {
+                        direction.Enqueue(1);
+                        nextDirection = 1;
+                    }
+                    else if (nextDirection != 3 && direction.Count == 1)
+                    {
+                        direction.Enqueue(1);
+                        nextDirection = 1;
+                    }
+                    break;
+                case Keys.Left:
+                    if (currentDirection != 0 && direction.Count == 0)
+                    {
+                        direction.Enqueue(2);
+                        nextDirection = 2;
+                    }
+                    else if (nextDirection != 0 && direction.Count == 1)
+                    {
+                        direction.Enqueue(2);
+                        nextDirection = 2;
+                    }
+                    break;
+                case Keys.Up:
+                    if (currentDirection != 1 && direction.Count == 0)
+                    {
+                        direction.Enqueue(3);
+                        nextDirection = 3;
+                    }
+                    else if (nextDirection != 1 && direction.Count == 1)
+                    {
+                        direction.Enqueue(3);
+                        nextDirection = 3;
+                    }
+                    break;
+                case Keys.Space:
+                    if (StartButton.Enabled != false)
+                        StartToolStripMenuItem_Click(null, null);
+                    break;
+                case Keys.Enter:
+                    if(StartButton.Enabled!=false)
+                        StartToolStripMenuItem_Click(null, null);
+                    break;
+            }
+            return true;
+        }
+
+        private void StartButton_Click(object sender, EventArgs e)
+        {
+            StartToolStripMenuItem_Click(null, null);
+        }
+        private void InitializeGame()
+        {
+            StartButton.Text = "开始";
+            StartToolStripMenuItem.Text = "开始";
+            StartButton.Enabled = true;
+            StartToolStripMenuItem.Enabled = true;
+            suspandTimes = 2;
+            run.Interval = 200;
+            HighestScoreBox.Text = GetRecord().ToString();
+            CurrentScoreBox.Text = "0";
+            LevelBox.Text = "1";
+            for (int i = 0; i < MAPSIZE; i++)
+                for (int j = 0; j < MAPSIZE; j++)
+                {
+                    map[i, j].BackColor = BACKGROUNDCOLOR;
+                }
+            head = MAPSIZE / 2 * 100 + MAPSIZE / 2;
+            body.Clear();
+            body.Enqueue(head);
+            map[head / 100, head % 100].BackColor = SNAKECOLOR;
+            body.Enqueue(++head);
+            map[head / 100, head % 100].BackColor = SNAKECOLOR;
+            direction.Clear();
+            currentDirection =0;
+            SetFood();
+        }
+
+        private void StartToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            if (run.Enabled)
+            {
+                StartButton.Text = "开始";
+                StartToolStripMenuItem.Text = "开始";
+                run.Enabled = false;
+                bgm.Stop();
+            }
+            else
+            {
+                StartButton.Text = "暂停";
+                StartToolStripMenuItem.Text = "暂停";
+                if (suspandTimes-- <= 0)
+                {
+                    StartButton.Enabled = false;
+                    StartToolStripMenuItem.Enabled = false;
+                }
+                run.Enabled = true;
+                bgm.Load();
+                bgm.PlayLooping();
+            }
+        }
+
+        private void EndToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void IntroductionToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("操控方向键让梦蛇吃到更多的食物，游戏中避免撞到障碍物。", "梦蛇", MessageBoxButtons.OK, MessageBoxIcon.Information);
+        }
+
+        private void AboutToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("此游戏为送给妹妹张梦宇的生日礼物。", "梦蛇", MessageBoxButtons.OK, MessageBoxIcon.Information);
+        }
+        private string FailShow()
+        {
+            string[] failShowArray = { "那个蛇快的没法！", "拿个小水喝杯子。", "妈剖呢？", "思蒙斯，思蒙斯，思蒙斯！", "那我呢？" ,"开着水，烧着门！", "捧捧脸，洗洗水！" , "谁不吃病谁有菜！", "干了一蒙子觉，睡了一蒙子活！", "一冰箱袜子，一洗衣机菜！" };
+            Random failShowIndex = new Random();
+            return failShowArray[failShowIndex.Next(0, failShowArray.Length)];
+        }
+    }
+}
